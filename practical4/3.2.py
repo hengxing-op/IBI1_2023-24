44b367599c9f5c32543ecd817c9d38455af03461
hengxing-op <hengxing.23@intl.zju.edu.cn>
2024-03-08 11:34:52 +0800

    edit 3.2

--- practical4/3.2.py
+++ practical4/3.2.py
@@ -2,6 +2,6 @@ X=1<2
 Y=1>2
 W= (X or Y) and (not X and Y)
 print (W)
-# When there are one true and one false, X or Y is true, X and Y is false, not X and Y is true, so (X or Y) and (not X and Y) is true, satisfies the condition.
-#When there are two trues, X or Y is true, but (not X and Y) is false, (X or Y) and (not X and Y) is false, satisfies the condition.
-#When there are two falses, X or Y is false, and (not X and Y)
+# When there are one true and one false, X or Y is true, X and Y is false, (not X and Y) is true, so (X or Y) and (not X and Y) is true, satisfies the requirement.
+#When there are two trues, X or Y is true, but (not X and Y) is false, (X or Y) and (not X and Y) is false, satisfies the requirement.
+#When there are two falses, X or Y is false, (not X and Y) is true, so () and () is false, satisfy the requirement.
