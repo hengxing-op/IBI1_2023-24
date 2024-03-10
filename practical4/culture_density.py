8954371ffe73f0783e1af0f175a4ba77f2500ec8
hengxing-op <hengxing.23@intl.zju.edu.cn>
2024-03-10 22:50:02 +0800

    edit culture_density

--- practical4/culture_density.py
+++ practical4/culture_density.py
@@ -10,4 +10,4 @@ while density<max_density:
     #double the density
     density=2*density
 #when the loop is broken, print which is that day and state the density is more than the max density)
-print ("On day",str(day),", the density of culture is over",str(max_density),"%")
+print ("On day",str(day),", the density of culture is over",str(max_density),"%",", so the day",str(day-1), "is the day before the cell density crosses 90%")
