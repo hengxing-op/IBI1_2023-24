8be4b8c9742c5bd75ad038463136a6066cb1135a
hengxing-op <hengxing.23@intl.zju.edu.cn>
2024-05-20 19:20:51 +0800

    edit practical 9

--- Practical_9/IBI1_student_database.py
+++ Practical_9/IBI1_student_database.py
@@ -1,14 +1,14 @@
 class Student:
-    def __init__(self, name, major, portfolio_score, project_score, exam_score):
-        self.name = name
-        self.major = major
-        self.portfolio_score = portfolio_score
-        self.project_score = project_score
-        self.exam_score = exam_score
+    def __init__(obj, name, major, portfolio_score, project_score, exam_score):
+        obj.name = name
+        obj.major = major
+        obj.portfolio_score = portfolio_score
+        obj.project_score = project_score
+        obj.exam_score = exam_score
     
-    def print_details(self):
-        print(f"Name: {self.name}, Major: {self.major}, Portfolio Score: {self.portfolio_score}, "
-              f"Project Score: {self.project_score}, Exam Score: {self.exam_score}")
+    def print_details(obj):
+        print(f"Name: {obj.name}, Major: {obj.major}, Portfolio Score: {obj.portfolio_score}, "
+              f"Project Score: {obj.project_score}, Exam Score: {obj.exam_score}")
 
 # Example of using the class
 student1 = Student("Qi Hengxing", "BMI", 95, 90, 88)  #Input position
